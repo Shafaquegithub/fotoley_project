d1549d5a09108165671787b4403bfe49e3b476f1
shafaqueimroz <imrozshafaque@gmail.com>
2022-07-31 02:25:21 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import { Grid } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import { Box } from '@mui/system';
+import data from './data';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import PauseIcon from '@mui/icons-material/Pause';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [activeItem, setActiveItem] = useState(0)
+  const len = data.length - 1
+  const [changeBtn, setChangeBtn] = useState(true)
+  
+  useEffect(()=>{
+    if(changeBtn == true){
+      const interval = setInterval(()=>{
+        setActiveItem(activeItem === len ? 0 : activeItem + 1)
+      }, 3000)
+     return () => clearInterval(interval)
+    }
+  }, [activeItem, changeBtn])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container className='main_grid'>
+        <span className='arrow left_arrow' onClick={() => setActiveItem(activeItem == 0 ? len : activeItem - 1)}><ArrowBackIosIcon style={{ fontSize: "20px" }} /></span>
+        <span className='arrow right_arrow' onClick={() => setActiveItem(activeItem == len ? 0 : activeItem + 1)}><ArrowForwardIosIcon style={{ fontSize: "20px" }} /></span>
+
+        <Grid item className='img_grid' xs={12} md={7}>
+          {
+            data.map((item, index) => (
+              <Box
+                component="img"
+                key={index}
+                sx={{
+                  height: "80%",
+                  width: "100%",
+                }}
+                alt="The house from the offer."
+                src={item.url}
+                style={{ borderRadius: "20px" }}
+                className={index == activeItem ? "active" : "inactive"}
+              />
+            ))
+          }
+
+          <Grid item className='small_img_grid'>
+
+            {
+              data.map((item, index) => (
+                <div style={{ height: "90%", width: "20%" }} key={index} onClick={()=>setActiveItem(index)}>
+                  <Box
+                    component="img"
+                    alt="The house from the offer."
+                    src={item.url}
+                    className={index == activeItem ? "nograyscale" : "grayscale"}
+                  />
+                </div>
+              ))
+            }
+          </Grid>
+        </Grid>
+        <Grid item xs={11} className="content_grid" md={5}>
+          {
+            data.map((item, index) => (
+              <div className={index == activeItem ? "active" : "inactive"} key={index}>
+                <div className='content_div'>
+                <Grid item className=' text_grid'>
+                  <Typography variant='h4' >
+                    {item.title}
+                  </Typography>
+                  <Typography variant='p' style={{ fontSize: "12px"}}>
+                   {item.description}
+                  </Typography>
+                </Grid>
+                <Grid item className='icon_div'>
+                  <span onClick={()=>setChangeBtn(!changeBtn)}>{changeBtn ==true ? <PlayArrowIcon style={{ fontSize: "28px" }} />:<PauseIcon  style={{ fontSize: "28px" }}/>}</span>
+                </Grid>
+                </div>
+                
+              </div>
+            ))
+          }
+        </Grid>
+
+      </Grid>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,40 @@
+export default [
+    {
+        title: "W Maldives",
+        description: "Maldives, officially the Republic of Maldives, is an archipelagic country located in Southern Asia, situated in the Indian Ocean. It lies southwest of Sri Lanka and India, about 750 kilometres from the Asian continent's mainland.Maldives, officially the Republic of Maldives, is an archipelagic country located in Southern Asia, situated in the Indian Ocean. It lies southwest of Sri Lanka and India, about 750 kilometres from the Asian continent's mainland.Maldives, officially the Republic of Maldives, is an archipelagic country located in Southern Asia, situated in the Indian Ocean. It lies southwest of Sri Lanka and India, about 750 kilometres.",
+        url:"https://images.thrillophilia.com/image/upload/s--kmjyV8C0--/c_fill,g_auto,h_600,q_auto,w_975/f_auto,fl_strip_profile/v1/images/photos/000/082/497/original/1634534424_radisson-blu-resort-maldives.jpg.jpg"
+    },
+    {
+        title: "Dubai Water Park",
+        description: "Here are some of the best water parks in Dubai: · Aquaventure Waterpark · Wild Wadi Waterpark · Yas Water Park · Legoland Water Park · IMG Worlds of AdventureHere are some of the best water parks in Dubai: · Aquaventure Waterpark · Wild Wadi Waterpark · Yas Water Park · Legoland Water Park · IMG Worlds of AdventureHere are some of the best water parks in Dubai: · Aquaventure Waterpark · Wild Wadi Waterpark · Yas Water Park · Legoland Water Park · IMG Worlds of AdventureHere are some of the best water parks in Dubai: · Aquaventure Waterpark · Wild Wadi Waterpark · Yas Water Park · Legoland Water Park · ",
+        url:"https://www.whitewaterwest.com/wp-content/uploads/fly-images/16717/1-Anaconda-Aquaventure-Waterpark-Atlantis-The-Palm-Dubai-UAE-2560x1436-c.jpg"
+    },
+    {
+        title: "Dubai Malls",
+        description: "Prices of UAE luxury goods are more in line with the US. But, it is still 15 to 25 per cent cheaper to purchase luxury goods in the UAE compared to China, South East Asia, the Indian Subcontinent and MENA countries.Prices of UAE luxury goods are more in line with the US. But, it is still 15 to 25 per cent cheaper to purchase luxury goods in the UAE compared to China, South East Asia, the Indian Subcontinent and MENA countries.  Prices of UAE luxury goods are more in line with the US. But, it is still 15 to 25 per cent cheaper to purchase luxury goods in the UAE compared to China, South East Asia, the Indian Subcontinent and MENA countries.        ",
+        url:"https://www.holidify.com/images/cmsuploads/compressed/shutterstock_228880075_20190822131717_20190822131816.jpg"
+    },
+    {
+        title: "Manali",
+        description: "Manali is a high-altitude Himalayan resort town in India’s northern Himachal Pradesh state. It has a reputation as a backpacking center and honeymoon destination. Set on the Beas River, it’s a gateway for skiing in the Solang Valley and trekking in Parvati Valley. It's also a jumping-off point for paragliding, rafting and mountaineering in the Pir Panjal mountains, home to 4,000m-high Rohtang Pass.Manali is a high-altitude Himalayan resort town in India’s northern Himachal Pradesh state. It has a reputation as a backpacking center and honeymoon destination. Set on the Beas River, it’s a gateway for skiing in the Solang Valley and trekking in Parvati Valley. ",
+        url:"https://img.theculturetrip.com/450x/smart/wp-content/uploads/2017/09/manali-1.jpg"
+    },
+    {
+        title: "Shimla",
+        description: "Shimla is the capital of the northern Indian state of Himachal Pradesh, in the Himalayan foothills. Once the summer capital of British India, it remains the terminus of the narrow-gauge Kalka-Shimla Railway, completed in 1903. It’s also known for the handicraft shops that line The Mall, a pedestrian avenue, as well as the Lakkar Bazaar, a market specializing in wooden toys and crafts.Shimla is the capital of the northern Indian state of Himachal Pradesh, in the Himalayan foothills. Once the summer capital of British India, it remains the terminus of the narrow-gauge Kalka-Shimla Railway, completed in 1903.",
+        url:"https://content.jdmagicbox.com/comp/def_content/swimming_pools/default-swimming-pools-9.jpg"
+    
+    },
+    // {
+    //     title: "Swimming Pools",
+    //     description: "A British mystery author visits her publisher's home in the South of France, where her interaction with his unusual daughter sets off some touchy dynamicsA British mystery author visits her publisher's home in the South of France, where her interaction with his unusual daughter sets off some touchy dynamics.A British mystery author visits her publisher's home in the South of France, where her interaction with his unusual daughter sets off some touchy dynamics.where her interaction with his unusual daughter sets off some touchy dynamics.",
+    // url:"https://img.traveltriangle.com/blog/wp-content/uploads/2017/10/cover-1.jpg"
+    
+    // },
+    // {
+    //     title: "Norway",
+    //     description: "Norway is a Scandinavian country encompassing mountains, glaciers and deep coastal fjords. Oslo, the capital, is a city of green spaces and museums. Preserved 9th-century Viking ships are displayed at Oslo’s Viking Ship Museum. Bergen, with colorful wooden houses, is the starting point for cruises to the dramatic Sognefjord. Norway is also known for fishing, hiking and skiing,Norway is a Scandinavian country encompassing mountains, glaciers and deep coastal fjords. Oslo, the capital, is a city of green spaces and museums. Preserved 9th-century Viking ships are displayed at Oslo’s Viking Ship Museum. Bergen, with colorful wooden houses, is the starting point for cruises to the dramatic Sognefjord. ",
+    //     url:"https://www.heartmybackpack.com/wp-content/uploads/2016/10/trond.jpg"
+    // }
+
+]
